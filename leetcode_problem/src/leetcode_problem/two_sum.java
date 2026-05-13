a3430c66b6aa05ff7a704e633e62775fccd042b4
BL-vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-05-13 17:16:13 +0530

    Added two_number code

---
+++ leetcode_problem/src/leetcode_problem/two_sum.java
@@ -0,0 +1,20 @@
+package leetcode_problem;
+
+public class two_sum {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5};
+		int target = 5;
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]+arr[j] == target) {
+					System.out.println(arr[i]);
+					System.out.println(arr[j]);
+					return;
+				}
+			}
+		}
+
+	}
+
+}
