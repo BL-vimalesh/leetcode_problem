0129f78acc5e8ecc61335dff3278204e215780ba
BL-vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-05-15 15:30:31 +0530

    Added queue problem code

---
+++ leetcode_problem/src/leetcode_problem/queue_program.java
@@ -0,0 +1,32 @@
+package leetcode_problem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
